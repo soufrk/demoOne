067cb91578cd47ae1f7d502ee44309b5713342cf
soufrk <soufrk@gmail.com>
2015-09-09 17:12:20 +0530

    Update Pattern004.java

--- SEDemos/src/pattern/Pattern004.java
+++ SEDemos/src/pattern/Pattern004.java
@@ -5,23 +5,36 @@ import java.util.regex.Pattern;
 
 /**
  * Additional Range Operation using ^, &&.
- * @author souvik.goswami
+ * 
+ * @author soufrk
  *
  */
 public class Pattern004 {
 
 	public static void main(String[] args) {
 		String input = "john o jo hn jon n j ohn ojhn john";
-		//Pattern pattern = Pattern.compile("[a-bA-B]");
-		Pattern pattern = Pattern.compile("john|ohn");
+		
+		Pattern pattern = Pattern.compile("[i-o]");
+		System.out.println(getResult(input, pattern, 
+				"Search for single character matching between 'i' till 'o'"));
+		
+		pattern = Pattern.compile("[i-o&&[^n]]");
+		System.out.println(getResult(input, pattern, 
+				"Search for single character matching between 'i' till 'o'"
+				+ " excluding 'n'"));
+	}
+
+	private static String getResult(String input, Pattern pattern, String desc) {
 		Matcher matcher = pattern.matcher(input);
-		String output = "Found match at:[";
-		while(matcher.find()){
+		String output = "Description: " + desc
+				+ "\nInput:" + input
+				+ "\nLength:" + input.length()
+				+ "\nFound at:[";
+		while (matcher.find()) {
 			output += matcher.start() + ",";
 		}
-		output = output.substring(0, output.length()-1) + "]";
-		System.out.println(output);
-
+		output = output.substring(0, output.length() - 1) + "]\n";
+		return output;
 	}
 
 }
