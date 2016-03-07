712ecfb237d3b9bc85f2e6a3ef866d23d0cb176b
soufrk <soufrk@gmail.com>
2016-03-07 14:38:49 +0530

    Added new package for inteface related examples.

---
+++ SEDemos/src/interfaces/Interface001.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+/**
+ * Access modifiers allowed in an interface.
+ * @author soufrk
+ *
+ */
+public interface Interface001 {
+	//private int getArea();
+	public float getVol(float x);
+	public void main(String [] args);
+	//public static void main(String [] args);
+	boolean setFlag(Boolean [] test);
+}
---
+++ SEDemos/src/interfaces/InterfaceConfusion.java
@@ -0,0 +1,52 @@
+package interfaces;
+
+/**
+ * What if same method is obtained from an interface and an Abstract class ??
+ * @author soufrk
+ *
+ */
+public class InterfaceConfusion {
+
+	public static void main(String args[]){
+		RealClass real = new RealClass();
+		real.go();
+	}
+}
+
+/**
+ * Simple interface
+ */
+interface AnInterface {
+	int i = 10;
+	void go();
+}
+
+/**
+ * Simple abstract class. 
+ */
+abstract class AnAbstractClass{
+	int i = 20;
+	//public abstract void go();
+	protected abstract void go();
+}
+
+/*abstract class AnAbstractClass implements AnInterface{
+	int i = 20;
+	//public abstract void go();
+	protected abstract void go();
+}*/
+
+/**
+ * Concrete class implements both interface & abstract class to create confusion. 
+ */
+class RealClass extends AnAbstractClass implements AnInterface{
+
+	@Override
+	public void go() {
+	//protected void go() {
+		//System.out.println(i);//The field i is ambiguous
+		System.out.println(AnInterface.i);
+		System.out.println(super.i);
+	}
+	
+}
