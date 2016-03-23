f0455ccfb09038382b51ef4c7dad6c1b1ac411bf
soufrk <soufrk@gmail.com>
2016-03-23 11:37:36 +0530

    Added new equality.

--- SEDemos/src/wrapper/WrapperConfusion.java
+++ SEDemos/src/wrapper/WrapperConfusion.java
@@ -11,13 +11,14 @@ public class WrapperConfusion {
 		
 		Integer i = 10;
 		Integer j = 10;
+		Integer m = 11;
 		//Integer l = 10;
 		System.out.println(i == j);
 		Integer k = new Integer(10);
 		i++;
 		System.out.println(i == j);
 		System.out.println(i == k);
-		
+		System.out.println(i == m);
 
 	}
 
