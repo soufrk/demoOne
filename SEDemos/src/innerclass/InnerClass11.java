8f2e251a777dc3afeb5e178f753a2caa21d536ea
Souvik <soufrk@gmail.com>
2016-03-27 18:21:15 +0530

    New Class

---
+++ SEDemos/src/innerclass/InnerClass11.java
@@ -0,0 +1,30 @@
+package innerclass;
+
+/**
+ * Anonymous types can be passed as argument in a method as well.
+ * @author soufrk
+ *
+ */
+public class InnerClass11 {
+
+	public static void main(String[] args) {
+		display(new MyInterface11() {
+			
+			@Override
+			public void display() {
+				System.out.println("Anonymous instance of MyInterface11 displays");
+				
+			}
+		});
+
+	}
+	
+	public static void display(MyInterface11 interface11){
+		interface11.display();
+	}
+
+}
+
+interface MyInterface11{
+	void display();
+}
