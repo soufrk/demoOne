e3b100caa651389bb8d85558073be0a48babee34
soufrk <soufrk@gmail.com>
2016-03-28 12:15:21 +0530

    Extended the example.

--- SEDemos/src/wrapper/WrapperConfusion.java
+++ SEDemos/src/wrapper/WrapperConfusion.java
@@ -9,6 +9,7 @@ public class WrapperConfusion {
 
 	public static void main(String[] args) {
 		
+		/* Notice how == behaves for Wrappers */
 		Integer i = 10;
 		Integer j = 10;
 		Integer m = 11;
@@ -20,6 +21,34 @@ public class WrapperConfusion {
 		System.out.println(i == k);
 		System.out.println(i == m);
 
+		/* The behaviour is partial here */
+		/*Integer a = 126;
+		Integer b = 127;
+		Integer c = 127;
+		Integer d = 128;
+		//b = b+1;
+		System.out.println(b == c);
+		a++;
+		System.out.println(a == b);
+		b++;
+		System.out.println(b == c);*/
+		
+		/* The behaviour is absolutely not observed here */
+		/*Integer w = 129;
+		Integer x = 130;
+		Integer y = 130;
+		Integer z = 131;
+		System.out.println(x == y);
+		w++;
+		System.out.println(w == x);
+		y++;
+		System.out.println(y == z);*/
+		
+		/*
+		 * Like String-pool, Integer primitive type also pools
+		 * some values. 
+		 */
+		
 	}
-
+	
 }
