bbff968a9344d264d5dc0b34a821b675089da384
soufrk <soufrk@gmail.com>
2016-03-29 17:21:50 +0530

    New package for abstract classes.

---
+++ SEDemos/src/abstractclasses/AbstractClass01.java
@@ -0,0 +1,14 @@
+package abstractclasses;
+
+/**
+ * Demonstrates that an abstract class need not have even a single method.
+ * 
+ * @author soufrk
+ *
+ */
+public class AbstractClass01 extends MyAbstract01 {
+
+}
+
+abstract class MyAbstract01 {
+}
