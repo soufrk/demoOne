1d008deff20dd178107a4e891b0d8ff208804163
soufrk <soufrk@gmail.com>
2016-03-31 15:47:18 +0530

    New classes with example.

---
+++ SEDemos/src/abstractclasses/AbstractClass03.java
@@ -0,0 +1,20 @@
+package abstractclasses;
+
+/**
+ * When implementing an interface, an abstract class,<ul>
+ * <li>may choose to not implement any methods at all</li>
+ * <li>may not unknowingly try to redefine an overriden
+ * method.</li> 
+ * </ul>
+ * @author soufrk
+ *
+ */
+public abstract class AbstractClass03 implements MyInterface03{
+	/*int show(){return 0;}*/
+
+}
+
+interface MyInterface03{
+	void show();
+	void display();
+}
