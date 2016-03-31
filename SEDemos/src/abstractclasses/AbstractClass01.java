66d38431175953482f1c2e6072c0db2cbf4d813b
soufrk <soufrk@gmail.com>
2016-03-31 15:46:32 +0530

    Changes to example.

--- SEDemos/src/abstractclasses/AbstractClass01.java
+++ SEDemos/src/abstractclasses/AbstractClass01.java
@@ -1,14 +1,24 @@
 package abstractclasses;
 
 /**
- * Demonstrates that an abstract class need not have even a single method.
+ * Demonstrates that an abstract class,<ul>
+ * <li>need not have even a single method.</li>
+ * <li>can have no abstract methods as well.</li>
+ * <li>can have only non-abstract methods only as well.</li>
+ * </ul>
  * 
  * @author soufrk
  *
  */
 public class AbstractClass01 extends MyAbstract01 {
-
+	public static void main(String args[]){
+		AbstractClass01 object = new AbstractClass01();
+		object.show();
+	}
 }
 
 abstract class MyAbstract01 {
+	void show(){
+		System.out.println("Hello World !!");
+	}
 }
