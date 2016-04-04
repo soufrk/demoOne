ca2866f0a3d4388e881401fc31c29b5a2869bf32
soufrk <soufrk@gmail.com>
2016-04-04 21:36:01 +0530

    Demonstrating array copy.

---
+++ SEDemos/src/array/Array01.java
@@ -0,0 +1,22 @@
+package array;
+
+public class Array01 {
+
+	public static void main(String[] args) {
+		int arr1[] = { 0, 1, 2, 3, 4, 5 };
+		int arr2[] = { 0, 10, 20, 30, 40, 50 };
+
+		// copies an array from the specified source array
+		System.arraycopy(arr1, 0, arr2, 0, 1);
+		System.out.println("Array 1");
+		for(int x:arr1){
+			System.out.print(x + " ");
+		}
+		System.out.println("\nArray 2");
+		for(int x:arr2){
+			System.out.print(x + " ");
+		}
+
+	}
+
+}
