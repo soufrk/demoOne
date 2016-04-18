91ef706c236fb7fb10053aeaeb9eec6c0c78bd5a
soufrk <soufrk@gmail.com>
2016-04-18 20:47:30 +0530

    Arrays.asList surprise !!

---
+++ SEDemos/src/array/Array03.java
@@ -0,0 +1,31 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * So let's create a list from an Array. Or shall we ??
+ * @author soufrk
+ *
+ */
+public class Array03 {
+
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,4,5};
+		List myList = Arrays.asList(arr);
+		
+		Iterator<Object> itr = myList.iterator();
+		while(itr.hasNext())
+			System.out.println(itr.next());
+		// Doesn't really print up the numbers is it ?
+		
+		myList = Arrays.asList(1, "A", 3, 4);
+		
+		itr = myList.iterator();
+		while(itr.hasNext())
+			System.out.println(itr.next());
+		
+	}
+
+}
