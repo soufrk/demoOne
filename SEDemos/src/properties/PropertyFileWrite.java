6c8b1ad1c8cd08a9ffaed0e3c61c221b85acb8fa
soufrk <soufrk@gmail.com>
2016-08-16 13:53:53 +0530

    New changes

---
+++ SEDemos/src/properties/PropertyFileWrite.java
@@ -0,0 +1,40 @@
+package properties;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
+public class PropertyFileWrite {
+
+	public static void main(String[] args) {
+		Properties prop = new Properties();
+		OutputStream output = null;
+
+		try {
+			output = new FileOutputStream("my.properties");
+
+			// set the properties value
+			prop.setProperty("key1", "localhost");
+			prop.setProperty("key2", "mkyong");
+			prop.setProperty("key3", "password");
+
+			// save properties to project root folder
+			prop.store(output, null);
+
+		} catch (IOException io) {
+			io.printStackTrace();
+		} finally {
+			if (output != null) {
+				try {
+					output.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+
+	}
+
+}
