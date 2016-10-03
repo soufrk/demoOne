eb38fa2174a268ae744b53bd3a27276c95105472
soufrk <soufrk@gmail.com>
2016-10-03 16:15:35 +0530

    Updated with formatted output.

--- SEDemos/src/test/PPFCalculator.java
+++ SEDemos/src/test/PPFCalculator.java
@@ -2,7 +2,7 @@ package test;
 
 public class PPFCalculator {
 	public static void main(String args[]) {
-		double[] p = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+		double[] p = {100.00, 100.00, 100.00, 100.00, 100.00, 100.00, 100.00, 100.00,  100.00, 100.00, 100.00, 100.00};
 		String[] m = { "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar" };
 		double[] i = { 8.1, 8.1, 8.1, 8.1, 8.1, 8.1, 8.1, 8.1, 8.1, 8.1, 8.0, 8.0, 8.0 };
 		double a = 0.0, total = 0.0;
@@ -10,9 +10,8 @@ public class PPFCalculator {
 			a += p[j];
 			double interest = a * i[j] / 1200;
 			total += interest;
-			System.out.println(m[j] + "-->" + interest);
+			System.out.format("%3s --> Amount:%8.2f Interest:%8.2f\n", m[j], a, interest);
 		}
-		System.out.println("Amount:" + a);
-		System.out.println("Total:" + total);
+		System.out.format(" Total Amount:%8.2f, Total Interest: %8.2f", a, total);
 	}
 }
