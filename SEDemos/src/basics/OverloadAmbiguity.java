681db00ba1b53962c43c84e5d6f95f851c556453
soufrk <soufrk@gmail.com>
2016-12-27 13:04:16 +0530

    Modified class.

--- SEDemos/src/basics/OverloadAmbiguity.java
+++ SEDemos/src/basics/OverloadAmbiguity.java
@@ -1,6 +1,12 @@
 package basics;
+/**
+ * Guess the compilation erros if any.
+ * @author soufrk
+ *
+ */
 
-class TestingClass {
+// Part 1
+public class OverloadAmbiguity {
 	public void test(String s) {
 		System.out.println("String");
 	}
@@ -16,5 +22,26 @@ class TestingClass {
 	public static void main(String[] args) {
 		TestingClass q = new TestingClass();
 		q.test(null);
+	}
+}
+
+// Part 2
+public class OverloadAmbiguity {
+	
+	public void test(String s) {
+		System.out.println("String");
+	}
+
+	public void test(String ...s) {
+		System.out.println("String list");
+	}
+	
+	public void test(String[] s) {
+		System.out.println("Object");
+	}
+
+	public static void main(String[] args) {
+		OverloadAmbiguity q = new OverloadAmbiguity();
+		q.test("Hello");
 	}
 }
