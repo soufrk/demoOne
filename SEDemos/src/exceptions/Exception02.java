6fe9db11e8d46f538558f8e2229107bade24af2a
soufrk <soufrk@gmail.com>
2017-01-17 11:24:15 +0530

    New class

---
+++ SEDemos/src/exceptions/Exception02.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+public class Exception02 extends Exception{
+	
+	public Exception02(String message){
+		super(message);
+	}
+
+	public static void main(String[] args) throws MyException {
+		throw new MyException();
+
+	}
+	
+	public static class MyException extends Exception02{
+		public MyException(){
+			super("Ooo111oo");
+		}
+	}
+
+}
