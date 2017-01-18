4bf1b5e9be0185742bf00050f187f3d0d54da03a
soufrk <soufrk@gmail.com>
2017-01-18 15:51:53 +0530

    New class.

---
+++ SEDemos/src/exceptions/Exception08.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+/**
+ * Demonstrates the combination of catch hierarchy.
+ * @author souvik.goswami
+ *
+ */
+public class Exception08 {
+
+	public static void main(String[] args) {
+		try {
+			int x = 0;
+			int y = 5 / x;
+		} catch (Throwable ae) {
+			System.out.println(" Throwable");
+		} catch (Exception e) {
+			System.out.println(" Exception");
+		} catch (RuntimeException ae) {
+			System.out.println(" Runtime Exception");
+		} catch (ArithmeticException ae) {
+			System.out.println(" Arithmetic Exception");
+		} 
+		System.out.println("finished");
+
+	}
+
+}
