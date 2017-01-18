435c420281e1d03f738f9910b8116313c8d5de5e
soufrk <soufrk@gmail.com>
2017-01-18 12:30:58 +0530

    New package for control flow.

---
+++ SEDemos/src/controls/LabelledBreak01.java
@@ -0,0 +1,32 @@
+package controls;
+
+public class LabelledBreak01 {
+
+	public static void main(String[] args) {
+		first: if (true) {
+			System.out.println("A");
+			break first;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		first: for (int i = 0; i < 3; i++) {
+			System.out.println(i);
+			if (i == 2)
+				break first;
+		}
+	}
+
+	public static void main(String[] args) {
+		first: 
+			for (int i = 0; i < 3; i++)
+			second: 
+				for (int j = 0; j < 3; j++) {
+				System.out.format("i=%d, j=%d\n", i, j);
+				if (j == 2)
+					break second;
+			}
+	}
+
+}
