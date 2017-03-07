091c9f722a2895a6320664fd5fccc4c135725d23
soufrk <soufrk@gmail.com>
2017-03-07 11:43:15 +0530

    Added implementations

--- SEDemos/src/exceptions/Exception01.java
+++ SEDemos/src/exceptions/Exception01.java
@@ -1,9 +1,34 @@
 package exceptions;
 
+import java.io.IOException;
+
+/**
+ * So, you think try-finally block works without a fuss.
+ * @author soufrk
+ *
+ */
 public class Exception01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try{
+			System.out.println("See");
+		} finally{
+			System.out.println("This works.");
+		}
+		
+		try{
+			System.out.println("And");
+			throw new IOException();
+		} finally{
+			System.out.println("This works as well.");
+		}
+		
+		try{
+			System.out.println("But");
+			throw new NullPointerException();
+		} finally{
+			System.out.println("Will this work ?");
+		}
 
 	}
 
