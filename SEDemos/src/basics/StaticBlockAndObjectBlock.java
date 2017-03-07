28dcb115228e90d9cc19da8a4310809f324b21a3
soufrk <soufrk@gmail.com>
2017-03-07 11:38:12 +0530

    New class demonstrating static & object block.

---
+++ SEDemos/src/basics/StaticBlockAndObjectBlock.java
@@ -0,0 +1,28 @@
+package basics;
+
+/**
+ * What is a static block and an object block ?
+ * @author soufrk
+ *
+ */
+public class StaticBlockAndObjectBlock {
+	
+	static{
+		System.out.println("Static");
+	}
+	
+	{
+		System.out.println("Object");
+	}
+	
+	public StaticBlockAndObjectBlock(){
+		System.out.println("Constructor");
+	}
+
+	public static void main(String[] args) {
+		new StaticBlockAndObjectBlock();
+		new StaticBlockAndObjectBlock();
+
+	}
+
+}
