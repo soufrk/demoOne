0e8dd554e73033712ed6948dad03df2952a76e0b
Souvik <soufrk@gmail.com>
2017-05-06 12:10:50 +0530

    Corrected code.

--- SEDemos/src/exceptions/Exception21.java
+++ SEDemos/src/exceptions/Exception21.java
@@ -12,10 +12,8 @@ public class Exception21 {
 	public static void main(String[] args) {
 		try(Scanner a = new Scanner(System.in);
 				System.out.println("Hello");
-				Scanner b = new Scanner(Scanner(Ssyem.in));){
-			
+				Scanner b = new Scanner(System.in);){
 		}
-
 	}
 
 }
