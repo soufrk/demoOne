e4b9a18d2f5466d3103eed40c3088f96b367f1af
Souvik <soufrk@gmail.com>
2017-05-06 12:31:41 +0530

    Intro to DirectoryStream.

---
+++ SEDemos/src/nio/DirStream.java
@@ -0,0 +1,35 @@
+package nio;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Use of blob strings in DirectoryStream to list files with a particular extension. 
+ * @author soufrk
+ *
+ */
+public class DirStream {
+
+	public static void main(String[] args) {
+
+		Path path = Paths.get("testDir");
+		/* Single extension blob. */
+		//try(DirectoryStream<Path> myStream = Files.newDirectoryStream(path.toAbsolutePath(), "*.txt")){
+		//try(DirectoryStream<Path> myStream = Files.newDirectoryStream(path.toAbsolutePath(), "*.java")){
+		
+		/* Compiles and executes without any output */
+		//try(DirectoryStream<Path> myStream = Files.newDirectoryStream(path.toAbsolutePath(), "*.java,*.txt")){
+		
+		/* Multiple extensions */
+		try(DirectoryStream<Path> myStream = Files.newDirectoryStream(path.toAbsolutePath(), "*.{java,txt}")){
+			for(Path myPath:myStream)
+				System.out.println(myPath);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+}
