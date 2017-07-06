c00e71c55af190de09a97f2c174aeca94ef54a60
Souvik Goswami <soufrk@gmail.com>
2017-07-06 13:16:01 +0530

    Access-specifier of a method-local inner-class

---
+++ SEDemos/src/innerclass/MethodLocalAccessSpecifier.java
@@ -0,0 +1,20 @@
+package innerclass;
+
+/**
+ * Guess which access-specifiers can be used in
+ * definition of a method-local inner-class.
+ * @author soufrk
+ *
+ */
+public class MethodLocalAccessSpecifier {
+
+	void show(){
+		class MyClassA{}
+		private class MyClassB{}
+		protected class MyClassC{}
+		public class MyClassD{}
+		abstract class MyClassE{}
+		final class MyClassF{}
+	}
+
+}
