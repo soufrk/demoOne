65f86e8c51569126eb9b1466f0d8eee84dbdff73
Souvik Goswami <soufrk@gmail.com>
2017-07-12 08:43:54 +0530

    Scope of method local inner class

---
+++ SEDemos/src/innerclass/MethodLocalInnerClassScope.java
@@ -0,0 +1,17 @@
+package innerclass;
+
+/**
+ * Scope of method-local inner class starts only
+ * post it's declaration.
+ * @author soufrk
+ *
+ */
+public class MethodLocalInnerClassScope {
+	
+	public void shaow(){
+		MyClass objA = new MyClass();
+		class MyClass{}
+		MyClass objB = new MyClass();
+	}
+
+}
