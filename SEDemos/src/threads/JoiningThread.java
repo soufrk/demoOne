955d1c36b0b4d983bfb8ddfd6db724b34b55d89c
Souvik Goswami <soufrk@gmail.com>
2017-07-16 13:16:14 +0530

    Abrupt introduction to join()

---
+++ SEDemos/src/threads/JoiningThread.java
@@ -0,0 +1,22 @@
+package threads;
+
+/**
+ * What's the harm in joining a Thread which hasn't yet been started.
+ * @author soufrk
+ *
+ */
+public class JoiningThread {
+
+	public static void main(String[] args) {
+		Thread t = new Thread(new Runnable(){
+			@Override
+			public void run() {
+				System.out.println("This is a separate thread.");
+			}
+		});
+		//t.start();
+		t.join();
+
+	}
+
+}
