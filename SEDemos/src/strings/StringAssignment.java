3ec469c666e8ba5651c1e7abd0b14b5c3420bd33
Souvik Goswami <soufrk@gmail.com>
2017-07-18 16:55:31 +0530

    Renamed class.

--- SEDemos/src/strings/StringAssignment.java
+++ SEDemos/src/strings/StringAssignment.java
@@ -1,18 +1,16 @@
 package strings;
 
 /**
- * Demonstrates a common mistake of variables
+ * Demonstrates a common mistake of variable assignment.
  * @author soufrk
  *
  */
-public class String002 {
-
-	public static void main(String[] args) {
-		String s1 = "a";
+public class StringAssignment {
+    public static void main(String[] args) {
+	String s1 = "a";
         String s2 = "d";
         String s3 = s2;
         s2 = "g";
         System.out.println(s1 + s2 + s3);
-	}
-
+    }
 }
