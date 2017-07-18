9bd21b5d37c410363138ad0bf726d74545d3de6f
Souvik Goswami <soufrk@gmail.com>
2017-07-18 13:50:41 +0530

    Battle of two maps.

---
+++ SEDemos/src/concurrency/ConcurrentVsHashMap.java
@@ -0,0 +1,26 @@
+package concurrency;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * ConcurrentHashMap Vs HashMap
+ * Find the odd method out.
+ * 
+ * @author soufrk
+ *
+ */
+public class ConcurrentVsHashMap {
+
+	public static void main(String[] args) {
+		// Given either of them,
+		ConcurrentHashMap<Integer, String> map = new ConcurrentHashMap<>();
+		HashMap<Integer, String> map = new HashMap<>();
+		// Find the odd one out.
+		map.put(1, "A");
+		map.putIfAbsent(2, "B");
+		map.remove(5);
+		map.removeIfPresent(6);
+	}
+
+}
