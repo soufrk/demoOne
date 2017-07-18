b33dbdbea3e6f2949dbc8f0977ad7b1f3343bd39
Souvik Goswami <soufrk@gmail.com>
2017-07-18 17:02:53 +0530

    Renamed and updated class

--- SEDemos/src/strings/String003.java
+++ SEDemos/src/strings/String003.java
@@ -1,17 +1,20 @@
 package strings;
 
+/**
+ * Is the behaviour of switch-case same as integer values.
+ * 
+ * @author soufrk
+ *
+ */
 public class String003 {
-
 	public static void main(String[] args) {
 		//String test = "A";
 		String test = "B";
 		switch (test){
-		case "A":System.out.println("Print A");
-		//default: System.out.println("Print C");
-		case "B":System.out.println("Print B");
-		default: System.out.println("Print C");
+		    case "A":System.out.println("Print A");
+		    default: System.out.println("Print C");
+		    case "B":System.out.println("Print B");
+		    default: System.out.println("Print C");
 		}
-
 	}
-
 }
