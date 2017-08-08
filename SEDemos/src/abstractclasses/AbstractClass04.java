474a7ddb25a66e1e4016135a4f8afbe6b2857d2a
Souvik Goswami <soufrk@gmail.com>
2017-08-08 17:07:31 +0530

    Udapted class, renamed

--- SEDemos/src/abstractclasses/AbstractClass04.java
+++ SEDemos/src/abstractclasses/AbstractClass04.java
@@ -2,20 +2,22 @@ package abstractclasses;
 
 /**
  * Can main method in an abstract class be executed ?
+ * Corollary, does an instances of class get created when 
+ * JVM executes main() ?
  * @author soufrk
  *
  */
-public abstract class AbstractClass04 extends Base{
+public abstract class AbstractClassMainMethod extends Base {
 
-	public static void main(String[] args) {
-		Base b = new Base();
-		b.print();
-	}
+  public static void main(String[] args) {
+    Base b = new Base();
+    b.print();
+  }
 
 }
 
-class Base{
-	public void print(){
-		System.out.println("Printing");
-	}
+class Base {
+  public void print() {
+    System.out.println("Printing");
+  }
 }
