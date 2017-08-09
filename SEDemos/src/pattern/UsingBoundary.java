b4472664ab6f803107c9ab20199ab6433604ed83
Souvik Goswami <soufrk@gmail.com>
2017-08-09 16:12:22 +0530

    Updated indentation

--- SEDemos/src/pattern/UsingBoundary.java
+++ SEDemos/src/pattern/UsingBoundary.java
@@ -11,33 +11,32 @@ import java.util.regex.Pattern;
  */
 public class UsingBoundary {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String[] input = {"a aa ab abc aab abb abc.", "abdc agfa cdab.", "abcd ABCD AbcD aBcD.",
-        "abcd012 ABCD123 AbcD234 aBcD345."};
-    Pattern[] patterns =
-        {Pattern.compile("[a,b]"), Pattern.compile("[ab]"), Pattern.compile("[a-c]"),
-            Pattern.compile("[a-bA-B]"), Pattern.compile("[a-bA-B]"), Pattern.compile("[a-b0-2]")};
-    prase(patterns, input);
-  }
-
-  private static void prase(Pattern[] patterns, String[] inputs) {
-    StringBuilder builder = new StringBuilder();
-    for (String input : inputs) {
-      builder.append("Input: " + input + "\n");
-      for (Pattern p : patterns) {
-        builder.append("Pattern: " + p + "\n");
-        Matcher matcher = p.matcher(input);
-        String output = "Found match at:[";
-        while (matcher.find()) {
-          output += matcher.start() + ",";
-        }
-        output = output.substring(0, output.length() - 1) + "]";
-        builder.append(output + "\n");
-      }
-      builder.append("\n");
+	String[] input = { "a aa ab abc aab abb abc.", "abdc agfa cdab.", "abcd ABCD AbcD aBcD.",
+		"abcd012 ABCD123 AbcD234 aBcD345." };
+	Pattern[] patterns = { Pattern.compile("[a,b]"), Pattern.compile("[ab]"), Pattern.compile("[a-c]"),
+		Pattern.compile("[a-bA-B]"), Pattern.compile("[a-bA-B]"), Pattern.compile("[a-b0-2]") };
+	prase(patterns, input);
     }
-    System.out.println(builder.toString());
-  }
+
+    private static void prase(Pattern[] patterns, String[] inputs) {
+	StringBuilder builder = new StringBuilder();
+	for (String input : inputs) {
+	    builder.append("Input: " + input + "\n");
+	    for (Pattern p : patterns) {
+		builder.append("Pattern: " + p + "\n");
+		Matcher matcher = p.matcher(input);
+		String output = "Found match at:[";
+		while (matcher.find()) {
+		    output += matcher.start() + ",";
+		}
+		output = output.substring(0, output.length() - 1) + "]";
+		builder.append(output + "\n");
+	    }
+	    builder.append("\n");
+	}
+	System.out.println(builder.toString());
+    }
 
 }
