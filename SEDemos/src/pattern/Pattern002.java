5a27953f5ed1b4e0ae373f209fc912fdd9ef1928
Souvik Goswami <soufrk@gmail.com>
2017-08-09 16:06:08 +0530

    Updated class. Minimized code.

--- SEDemos/src/pattern/Pattern002.java
+++ SEDemos/src/pattern/Pattern002.java
@@ -6,59 +6,38 @@ import java.util.regex.Pattern;
 /**
  * Demonstrates use of ranges of pattern using [].
  * 
- * @author soufrk
+ * @author soufrk.
  *
  */
 public class Pattern002 {
 
-  public static void main1(String[] args) {
-    String input = "a aa ab abc aab abb abc.";
-    Pattern pattern = Pattern.compile("[a,b]");
-    // Pattern pattern = Pattern.compile("[ab]");
-    Matcher matcher = pattern.matcher(input);
-    String output = "Found match at:[";
-    while (matcher.find()) {
-      output += matcher.start() + ",";
-    }
-    output = output.substring(0, output.length() - 1) + "]";
-    System.out.println(output);
+  public static void main(String[] args) {
+
+    String[] input = {"a aa ab abc aab abb abc.", "abdc agfa cdab.", "abcd ABCD AbcD aBcD.",
+        "abcd012 ABCD123 AbcD234 aBcD345."};
+    Pattern[] patterns =
+        {Pattern.compile("[a,b]"), Pattern.compile("[ab]"), Pattern.compile("[a-c]"),
+            Pattern.compile("[a-bA-B]"), Pattern.compile("[a-bA-B]"), Pattern.compile("[a-b0-2]")};
+    prase(patterns, input);
   }
 
-  public static void main2(String[] args) {
-    String input = "abdc agfa cdab.";
-    Pattern pattern = Pattern.compile("[a-c]");
-    Matcher matcher = pattern.matcher(input);
-    String output = "Found match at:[";
-    while (matcher.find()) {
-      output += matcher.start() + ",";
+  private static void prase(Pattern[] patterns, String[] inputs) {
+    StringBuilder builder = new StringBuilder();
+    for (String input : inputs) {
+      builder.append("Input: " + input + "\n");
+      for (Pattern p : patterns) {
+        builder.append("Pattern: " + p + "\n");
+        Matcher matcher = p.matcher(input);
+        String output = "Found match at:[";
+        while (matcher.find()) {
+          output += matcher.start() + ",";
+        }
+        output = output.substring(0, output.length() - 1) + "]";
+        builder.append(output + "\n");
+      }
+      builder.append("\n");
     }
-    output = output.substring(0, output.length() - 1) + "]";
-    System.out.println(output);
-  }
-
-  public static void main3(String[] args) {
-    String input = "abcd ABCD AbcD aBcD.";
-    Pattern pattern = Pattern.compile("[a-bA-B]");
-    Matcher matcher = pattern.matcher(input);
-    String output = "Found match at:[";
-    while (matcher.find()) {
-      output += matcher.start() + ",";
-    }
-    output = output.substring(0, output.length() - 1) + "]";
-    System.out.println(output);
-  }
-
-  public static void main4(String[] args) {
-    String input = "abcd012 ABCD123 AbcD234 aBcD345.";
-    // Pattern pattern = Pattern.compile("[a-bA-B]");
-    Pattern pattern = Pattern.compile("[a-b0-2]");
-    Matcher matcher = pattern.matcher(input);
-    String output = "Found match at:[";
-    while (matcher.find()) {
-      output += matcher.start() + ",";
-    }
-    output = output.substring(0, output.length() - 1) + "]";
-    System.out.println(output);
+    System.out.println(builder.toString());
   }
 
 }
