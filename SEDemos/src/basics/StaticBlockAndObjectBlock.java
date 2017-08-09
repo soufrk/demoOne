8142cf5ad2fb111da8e35b8751fb28182a0e9e11
Souvik Goswami <soufrk@gmail.com>
2017-08-09 15:25:58 +0530

    Changed indentation

--- SEDemos/src/basics/StaticBlockAndObjectBlock.java
+++ SEDemos/src/basics/StaticBlockAndObjectBlock.java
@@ -2,27 +2,27 @@ package basics;
 
 /**
  * What is a static block and an object block ?
+ * 
  * @author soufrk
  *
  */
 public class StaticBlockAndObjectBlock {
-	
-	static{
-		System.out.println("Static");
-	}
-	
-	{
-		System.out.println("Object");
-	}
-	
-	public StaticBlockAndObjectBlock(){
-		System.out.println("Constructor");
-	}
 
-	public static void main(String[] args) {
-		new StaticBlockAndObjectBlock();
-		new StaticBlockAndObjectBlock();
+  static {
+    System.out.println("Static");
+  }
 
-	}
+  {
+    System.out.println("Object");
+  }
+
+  public StaticBlockAndObjectBlock() {
+    System.out.println("Constructor");
+  }
+
+  public static void main(String[] args) {
+    new StaticBlockAndObjectBlock();
+    new StaticBlockAndObjectBlock();
+  }
 
 }
