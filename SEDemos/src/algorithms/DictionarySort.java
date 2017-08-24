81629d7af5aabdd62aef2d72bf95b153c418f2b3
Souvik <soufrk@gmail.com>
2017-08-24 20:37:21 +0530

    Updated class formatting.

--- SEDemos/src/algorithms/DictionarySort.java
+++ SEDemos/src/algorithms/DictionarySort.java
@@ -6,22 +6,23 @@ import java.util.TreeSet;
 
 /**
  * Implementation of a dictionary like sorting of words ina given String.
+ * 
  * @author soufrk
  *
  */
 public class DictionarySort {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		Integer numberOfStrings = scanner.nextInt();
-		Set<String> setOfStrings = new TreeSet<>();
-		while(numberOfStrings-->0){
-			setOfStrings.add(scanner.next());
-		}
-		scanner.close();
-		for(String temp:setOfStrings){
-			System.out.println(temp);
-		}
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	Integer numberOfStrings = scanner.nextInt();
+	Set<String> setOfStrings = new TreeSet<>();
+	while (numberOfStrings-- > 0) {
+	    setOfStrings.add(scanner.next());
 	}
+	scanner.close();
+	for (String temp : setOfStrings) {
+	    System.out.println(temp);
+	}
+    }
 
 }
--- SEDemos/src/algorithms/IndexedUniqueElementFinder.java
+++ SEDemos/src/algorithms/IndexedUniqueElementFinder.java
@@ -4,77 +4,75 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Find the 3rd unique character in a given String. 
+ * Find the 3rd unique character in a given String.
+ * 
  * @author soufrk
  *
  */
 public class IndexedUniqueElementFinder {
 
-	public static void main(String[] args) {
-		
-		String input = "abcedb";
-		method1(input);
+    public static void main(String[] args) {
+	String inputs[] = {"aabcd", "abcedb"};
+	for(String input:inputs)
+	    method1(input);
+    }
 
+    public static /* char */void method1(String input) {
+	Map<Character, CharOccuranceIndex> mapOfCharacters = new HashMap<>();
+	int index = 0;
+	for (char ch : input.toCharArray()) {
+	    if (mapOfCharacters.containsKey(ch)) {
+		CharOccuranceIndex value = mapOfCharacters.get(ch);
+		value.incrementOccurance();
+	    } else {
+		CharOccuranceIndex value = new CharOccuranceIndex(ch, index++);
+		mapOfCharacters.put(ch, value);
+	    }
 	}
-	
-	public static /*char*/void method1(String input){
-		Map<Character, CharOccuranceIndex> mapOfCharacters = new HashMap<>();
-		int index = 0;
-		for(char ch:input.toCharArray()){
-			if(mapOfCharacters.containsKey(ch)){
-				CharOccuranceIndex value = mapOfCharacters.get(ch);
-				value.incrementOccurance();
-			} else{
-				CharOccuranceIndex value = new CharOccuranceIndex(ch, index++);
-				mapOfCharacters.put(ch, value);
-			}
-		}
-		System.out.println(mapOfCharacters);
-	}
-	
-	private static class CharOccuranceIndex{
-		
-		char ch;
-		int index;
-		int occurance;
-		
-		public CharOccuranceIndex(char ch, int index){
-			this.ch = ch;
-			this.index = index;
-			occurance = 1;
-		}
-		
-		public void incrementOccurance(){
-			this.occurance ++;
-		}
-		
-		public boolean isUnique(){
-			return occurance>1?true:false;
-		}
-		
-		@Override
-		public int hashCode(){
-			return ch;
-		}
-		
-		@Override
-		public boolean equals(Object object){
-			boolean result = false;
-			if(this == object) result = true;
-			else if(this == null || object.getClass() != this.getClass()){
-				
-			} else {
-				result = this.ch == ((CharOccuranceIndex)object).ch;
-			}
-			return result;
-		}
+	System.out.println(mapOfCharacters + "\n" + "=======================");
+    }
 
-		@Override
-		public String toString() {
-			return "CharOccuranceIndex [ch=" + ch + ", index=" + index + ", occurance=" + occurance + "]\n";
-		}
-		
+    private static class CharOccuranceIndex {
+	char ch;
+	int index;
+	int occurance;
+
+	public CharOccuranceIndex(char ch, int index) {
+	    this.ch = ch;
+	    this.index = index;
+	    occurance = 1;
 	}
+
+	public void incrementOccurance() {
+	    this.occurance++;
+	}
+
+	public boolean isUnique() {
+	    return occurance > 1 ? true : false;
+	}
+
+	@Override
+	public int hashCode() {
+	    return ch;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+	    boolean result = false;
+	    if (this == object)
+		result = true;
+	    else if (this == null || object.getClass() != this.getClass()) {
+
+	    } else {
+		result = this.ch == ((CharOccuranceIndex) object).ch;
+	    }
+	    return result;
+	}
+
+	@Override
+	public String toString() {
+	    return "CharOccuranceIndex [ch=" + ch + ", index=" + index + ", occurance=" + occurance + "]\n";
+	}
+    }
 
 }
-
