cb5e82fb8e4cecb0ee640c96244b467ef7adcbac
Souvik <soufrk@gmail.com>
2017-08-24 20:17:09 +0530

    Updated class with formatting.

--- SEDemos/src/collections/RawTreeSet.java
+++ SEDemos/src/collections/RawTreeSet.java
@@ -4,26 +4,27 @@ import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * The question is not if the code compiles or gives output.
- * The code generates a ClassCastException.
+ * The question is not if the code compiles or gives output. 
+ * Given that the code generates a ClassCastException,
  * 
- * The question is what will be printed as Exception message.
+ * what will be printed as exception-message ?
+ * 
  * @author soufrk
  *
  */
 public class RawTreeSet {
 
-	public static void main(String[] args) {
-		Set mySet = new TreeSet();
-		
-		// Case 1
-		mySet.add(1);
-		mySet.add("C");
-		
-		// Case 2
-		mySet.add("C");
-		mySet.add("B");
-		mySet.add(1);
-	}
+    public static void main(String[] args) {
+	Set mySet = new TreeSet();
+
+	// Case 1
+	mySet.add(1);
+	mySet.add("C");
+
+	// Case 2
+	mySet.add("C");
+	mySet.add("B");
+	mySet.add(1);
+    }
 
 }
--- SEDemos/src/collections/ReferenceConfusion.java
+++ SEDemos/src/collections/ReferenceConfusion.java
@@ -7,71 +7,72 @@ import java.util.Map;
 
 /**
  * Importance of immutability of a class, when used with collections.
+ * 
  * @author soufrk
  *
  */
 public class ReferenceConfusion {
 
-	public static void main(String[] args) {
-		
-		List<Integer> myList = new LinkedList<>();
-		Integer integer = null;
-		for(int i=0;i<10;i++){
-			integer = i;
-			myList.add(i);
-		}
-		System.out.println(myList);
-		
-		Map<Integer, String> myMap = new HashMap<>();
-		Integer i = 10;
-		String s = "A";
-		myMap.put(i, s);
-		i = 11;
-		s = "B";
-		myMap.put(i, s);
-		System.out.println(myMap);
-		
-		class MyClass{
-			int id;
+    public static void main(String[] args) {
 
-			public MyClass(int id) {
-				super();
-				this.id = id;
-			}
-
-			@Override
-			public String toString() {
-				return "MyClass [id=" + id + "]";
-			}
-
-			public int getId() {
-				return id;
-			}
-
-			public void setId(int id) {
-				this.id = id;
-			}
-			
-		}
-		
-		Map<Integer, MyClass> myMap = new HashMap<>();
-		Integer i = 10;
-		MyClass s = new MyClass(1);
-		myMap.put(i, s);
-		s.setId(2);
-		i = 11;
-		myMap.put(i, s);
-		s.setId(13);
-		System.out.println(myMap);
-		
-		List<MyClass> myList = new LinkedList<>();
-		MyClass s = new MyClass(1);
-		for(int i=0;i<10;i++){
-			myList.add(s);
-		}
-		System.out.println(myList);
-		s.setId(11);
-		System.out.println(myList);
+	List<Integer> myList = new LinkedList<>();
+	Integer integer = null;
+	for (int i = 0; i < 10; i++) {
+	    integer = i;
+	    myList.add(i);
 	}
-	
+	System.out.println(myList);
+
+	Map<Integer, String> myMap = new HashMap<>();
+	Integer i = 10;
+	String s = "A";
+	myMap.put(i, s);
+	i = 11;
+	s = "B";
+	myMap.put(i, s);
+	System.out.println(myMap);
+
+	class MyClass {
+	    int id;
+
+	    public MyClass(int id) {
+		super();
+		this.id = id;
+	    }
+
+	    @Override
+	    public String toString() {
+		return "MyClass [id=" + id + "]";
+	    }
+
+	    public int getId() {
+		return id;
+	    }
+
+	    public void setId(int id) {
+		this.id = id;
+	    }
+
+	}
+
+	Map<Integer, MyClass> myMap = new HashMap<>();
+	Integer i = 10;
+	MyClass s = new MyClass(1);
+	myMap.put(i, s);
+	s.setId(2);
+	i = 11;
+	myMap.put(i, s);
+	s.setId(13);
+	System.out.println(myMap);
+
+	List<MyClass> myList = new LinkedList<>();
+	MyClass s = new MyClass(1);
+	for (int i = 0; i < 10; i++) {
+	    myList.add(s);
+	}
+	System.out.println(myList);
+	s.setId(11);
+	System.out.println(myList);
+    }
+
 }
