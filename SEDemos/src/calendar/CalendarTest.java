bd4a3d6e6e8b3aa73f979569977caf0b779fcf80
Souvik <soufrk@gmail.com>
2017-08-24 18:50:40 +0530

    Formatted class.

--- SEDemos/src/calendar/CalendarTest.java
+++ SEDemos/src/calendar/CalendarTest.java
@@ -6,13 +6,12 @@ import java.util.Locale;
 
 public class CalendarTest {
 
-	public static void main(String[] args) {
-		Formatter formatter = new Formatter();
-		Calendar calendar = Calendar.getInstance(Locale.US);
-		calendar.set(2012, Calendar.FEBRUARY, 1);
-		formatter.format("%tY/%tm/%td", calendar, calendar, calendar, calendar);
-		System.out.println(formatter);
-
-	}
+    public static void main(String[] args) {
+	Formatter formatter = new Formatter();
+	Calendar calendar = Calendar.getInstance(Locale.US);
+	calendar.set(2012, Calendar.FEBRUARY, 1);
+	formatter.format("%tY/%tm/%td", calendar, calendar, calendar, calendar);
+	System.out.println(formatter);
+    }
 
 }
