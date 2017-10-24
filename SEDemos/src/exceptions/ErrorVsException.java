96cbff33cff9f6168bf376838dfee0806d6c6920
Souvik Goswami <soufrk@gmail.com>
2017-10-24 14:49:27 +0530

    New class.

---
+++ SEDemos/src/exceptions/ErrorVsException.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+/**
+ * Exception and Error have different hierarchy.
+ * @author soufrk
+ *
+ */
+public class ErrorVsException {
+
+    public static void main(String[] args) {
+	try{
+	    throw new StackOverflowError();
+	} catch(Exception e){
+	    System.out.println("Caught in fisrt");
+	} catch(Throwable e){
+	    System.out.println("Caught in second");
+	}
+    }
+
+}
