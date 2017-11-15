172c58ac146c66f276a2dbacc3787c8b1ffa3567
Souvik Goswami <soufrk@gmail.com>
2017-11-15 16:24:34 +0530

    Modified the example.

--- SEDemos/src/array/ArraySortOrNot.java
+++ SEDemos/src/array/ArraySortOrNot.java
@@ -3,21 +3,29 @@ package array;
 import java.util.Arrays;
 
 /**
- * Guess what happens in the two sorting scenarios.
+ * Guess what happens in these sorting scenarios.
  * 
  * @author soufrk
  *
  */
 
 public class ArraySortOrNot {
+    
+    public static void main(String[] args) {
+	String []arr = {"Z", "Y", "P", "T", "M"};
+	Arrays.sort(arr, null);
+	for(String i:arr)
+	    System.out.println(i + " ");
+    }
 
     public static void main1(String[] args) {
 	int []arr = {1, 0, 7, 6, 3};
-	//Arrays.sort(arr, null);
-	System.out.println(arr);
+	Arrays.sort(arr, null);
+	for(int i:arr)
+	    System.out.println(i + " ");
     }
     
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
 	int []arr = {1, 0, 7, 6, 3};
 	Arrays.sort(arr);
 	for(int i:arr)
