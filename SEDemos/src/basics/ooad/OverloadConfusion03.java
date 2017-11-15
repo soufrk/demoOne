c62e045f76f7532d4e03881942eb842b56586e35
Souvik Goswami <soufrk@gmail.com>
2017-11-15 13:55:30 +0530

    Added new overload illustration.

---
+++ SEDemos/src/basics/ooad/OverloadConfusion03.java
@@ -0,0 +1,53 @@
+package basics.ooad;
+
+/**
+ * Guess the ouput. Hint: guess what happens if we comment all methods, and
+ * start uncommenting from bottom one to top.
+ * 
+ * @author soufrk
+ *
+ */
+public class OverloadConfusion03 {
+
+    public static void main(String[] args) {
+	MyInterface first = new Implementation1();
+	Implementation2 second = new Implementation2();
+	method(first);
+	method((Object) first);
+	method(second);
+	method((MyInterface) second);
+    }
+
+    public static void method(Implementation1 i) {
+	System.out.println(i.getName());
+    }
+
+    public static void method(Implementation2 i) {
+	System.out.println(i.getName());
+    }
+
+    public static void method(MyInterface s) {
+	System.out.println("Interface");
+    }
+
+    public static void method(Object o) {
+	System.out.println("Object");
+    }
+
+}
+
+interface MyInterface {
+    String getName();
+}
+
+class Implementation1 implements MyInterface {
+    public String getName() {
+	return "Implementation1";
+    }
+}
+
+class Implementation2 implements MyInterface {
+    public String getName() {
+	return "Implementation2";
+    }
+}
