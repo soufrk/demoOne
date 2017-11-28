4469376dc8533afc769a94141fbfcd014c7f4616
Souvik Goswami <soufrk@gmail.com>
2017-11-28 13:35:07 +0530

    Added new class

---
+++ SEDemos/src/nio/SubpathOfPath.java
@@ -0,0 +1,31 @@
+package nio;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Various demonstartions with subpath() method.
+ * @author soufrk
+ *
+ */
+public class SubpathOfPath {
+    static final Path PATH = Paths.get("A/B/C/D/E/F/G");
+
+    /* Positive examples. Guess the ouput. */
+    public static void main1(String[] args) {
+	System.out.println(PATH.subpath(0, 1));
+	System.out.println(PATH.subpath(0, 2));
+	System.out.println(PATH.subpath(0, 7));
+	System.out.println(PATH.subpath(0, 6));
+    }
+    
+    /* Negative examples. Guess the exception */
+    public static void main(String[] args) {
+	//System.out.println(PATH.subpath(0, 0));
+	//System.out.println(PATH.subpath(-1, 0));
+	//System.out.println(PATH.subpath(0, -1));
+	//System.out.println(PATH.subpath(1, 0));
+	System.out.println(PATH.subpath(0, 8));
+    }
+    
+}
