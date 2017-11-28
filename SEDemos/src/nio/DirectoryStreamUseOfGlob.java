776106a149f476fc36b6a5a789f39240a3ae244d
Souvik Goswami <soufrk@gmail.com>
2017-11-28 18:46:22 +0530

    Modified files.

---
+++ SEDemos/src/nio/DirectoryStreamUseOfGlob.java
@@ -0,0 +1,48 @@
+package nio;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Use of glob strings in DirectoryStream to list files with a particular extension. 
+ * @author soufrk
+ *
+ */
+public class DirectoryStreamUseOfGlob {
+
+    public static void main(String[] args) {
+	
+	Path path = Paths.get(".");
+	String[] simpleExtensions = {"*.txt", "*.java"};
+	String[] multipleExtensions = {"*.htm,*.txt", "*.{java,txt}"};
+	String[] nameAndExtension = {"intel*.txt"};
+	
+	/* Single extension blob. */
+	printDir(path.toAbsolutePath(), simpleExtensions[0]);
+	printDir(path.toAbsolutePath(), simpleExtensions[1]);
+	
+	/* Compiles and executes without any output */
+	printDir(path.toAbsolutePath(), multipleExtensions[0]);
+	/* Multiple extensions */
+	//try(DirectoryStream<Path> myStream = Files.newDirectoryStream(path.toAbsolutePath(), )){
+	
+	/* Name matching pattern */
+	//try(DirectoryStream<Path> myStream = Files.newDirectoryStream(path.toAbsolutePath(), "intel*.txt")){
+	
+	/* Name and extension match. */
+	
+    }
+    
+    public static void printDir(Path path, String glob){
+	try(DirectoryStream<Path> myStream = Files.newDirectoryStream(path.toAbsolutePath(), glob)){
+	    for(Path myPath:myStream)
+		System.out.println(myPath);
+	}catch(IOException e){
+	    e.printStackTrace();
+	}
+    }
+
+}
--- SEDemos/src/nio/filewatcher/WatcherDemo1.java
+++ SEDemos/src/nio/filewatcher/WatcherDemo1.java
@@ -13,53 +13,52 @@ import java.nio.file.WatchService;
 import java.util.Scanner;
 
 /**
- * Simple FileWatcher, that automatically reads any file, if there is any I/O operation 
- * like create, delete, apeends.
- * Assumed there is a sinlge file under directory being observed, and I/O event is 
- * append always.
+ * Simple FileWatcher, that automatically reads any file, if there is any I/O
+ * operation like create, delete, appends. Assumed there is a single file under
+ * directory being observed, and I/O event is append always.
  * 
- * @author souvik.goswami
+ * @author soufrk
  *
  */
 public class WatcherDemo1 {
 
-	public static void main(String[] args) {
-		Path dir = Paths.get("").toAbsolutePath();
+    public static void main(String[] args) {
+	Path dir = Paths.get("").toAbsolutePath();
+	try {
+	    WatchService watcher = FileSystems.getDefault().newWatchService();
+	    WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+	    while (true) {
+		WatchKey innerKey;
 		try {
-			WatchService watcher = FileSystems.getDefault().newWatchService();
-			WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
-			while (true) {
-				WatchKey innerKey;
-				try {
-					key = watcher.take();
-				} catch (InterruptedException x) {
-					return;
-				}
-				for (WatchEvent<?> event : key.pollEvents()) {
-					WatchEvent.Kind<?> kind = event.kind();
-					if (kind == OVERFLOW) {
-						continue;
-					}
-					WatchEvent<Path> ev = (WatchEvent<Path>) event;
-					Path filename = ev.context();
-					System.out.println("Event triggered by:" + filename);
-					try (Scanner scanner = new Scanner(Files.newBufferedReader(filename));) {
-						while (scanner.hasNextLine()) {
-							System.out.println(scanner.nextLine());
-						}
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-
-					boolean valid = key.reset();
-					if (!valid) {
-						break;
-					}
-				}
-			}
-		} catch (IOException x) {
-			System.err.println(x);
+		    key = watcher.take();
+		} catch (InterruptedException x) {
+		    return;
 		}
+		for (WatchEvent<?> event : key.pollEvents()) {
+		    WatchEvent.Kind<?> kind = event.kind();
+		    if (kind == OVERFLOW) {
+			continue;
+		    }
+		    WatchEvent<Path> ev = (WatchEvent<Path>) event;
+		    Path filename = ev.context();
+		    System.out.println("Event triggered by:" + filename);
+		    try (Scanner scanner = new Scanner(Files.newBufferedReader(filename));) {
+			while (scanner.hasNextLine()) {
+			    System.out.println(scanner.nextLine());
+			}
+		    } catch (IOException e) {
+			e.printStackTrace();
+		    }
+
+		    boolean valid = key.reset();
+		    if (!valid) {
+			break;
+		    }
+		}
+	    }
+	} catch (IOException x) {
+	    System.err.println(x);
 	}
+    }
 
 }
