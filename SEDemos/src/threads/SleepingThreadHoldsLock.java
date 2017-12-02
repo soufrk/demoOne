38403c5d80843582ae7bccc80e8c995b9a0a68be
Souvik Goswami <soufrk@gmail.com>
2017-12-02 14:55:40 +0530

    Added new file

---
+++ SEDemos/src/threads/SleepingThreadHoldsLock.java
@@ -0,0 +1,48 @@
+package threads;
+
+/**
+ * Let's put an end to if a sleeping-thread holds a lock
+ * or not.
+ * 
+ * @author soufrk
+ *
+ */
+public class SleepingThreadHoldsLock extends Thread {
+
+    public static void main(String[] args) {
+	Thread t1 = new TheSleepyThread();
+	Thread t2 = new Thread(new TheNonSleepyThread());
+	t1.start();
+	t2.start();
+    }
+    
+    private static class TheSleepyThread extends Thread {
+	@Override
+	public void run() {
+	    synchronized (SleepingThreadHoldsLock.class) {
+		try {
+		    sleep(10000);
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		}
+	    }
+	}
+    }
+    
+    private static class TheNonSleepyThread implements Runnable {
+	@Override
+	public void run() {
+	    synchronized (SleepingThreadHoldsLock.class) {
+		try {
+		    wait(2000);
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		} catch (IllegalMonitorStateException e){
+		    System.out.println("Unable to acquire lock !!!!!");
+		    e.printStackTrace();
+		}
+	    }
+	}
+    }
+
+}
