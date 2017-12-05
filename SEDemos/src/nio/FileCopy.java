285de0a1be537f4f22a7cb7ff4b4cf0b0979a4c9
Souvik Goswami <soufrk@gmail.com>
2017-12-05 17:34:02 +0530

    Corrected code indentation.

--- SEDemos/src/nio/FileCopy.java
+++ SEDemos/src/nio/FileCopy.java
@@ -11,7 +11,8 @@ import java.nio.file.StandardCopyOption;
  * <ul>
  * <li>with simple paths compiles and executes nothing.</li>
  * <li>only with actual source path, creates copy.</li>
- * <li>if source is directory, creates a blank directory with destination name.</li>
+ * <li>if source is directory, creates a blank directory with destination
+ * name.</li>
  * <li>if source is file, creates a blank directory with destination name.</li>
  * </ul>
  * 
@@ -20,24 +21,24 @@ import java.nio.file.StandardCopyOption;
  */
 public class FileCopy {
 
-	/*
-	 * public static void main(String[] args) { Path path =
-	 * Paths.get("testDir"); Path emptyDest = Paths.get("dest1"); try {
-	 * Files.copy(path, emptyDest, StandardCopyOption.REPLACE_EXISTING); } catch
-	 * (IOException e) { e.printStackTrace(); }
-	 * 
-	 * }
-	 */
-
-	public static void main(String[] args) {
-		Path path = Paths.get("testDir", "a.txt");
-		Path emptyDest = Paths.get("dest1");
-		try {
-			Files.copy(path.toAbsolutePath(), emptyDest, StandardCopyOption.REPLACE_EXISTING);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+    public static void main1(String[] args) {
+	Path path = Paths.get("testDir");
+	Path emptyDest = Paths.get("dest1");
+	try {
+	    Files.copy(path, emptyDest, StandardCopyOption.REPLACE_EXISTING);
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+    }
+
+    public static void main2(String[] args) {
+	Path path = Paths.get("testDir", "a.txt");
+	Path emptyDest = Paths.get("dest1");
+	try {
+	    Files.copy(path.toAbsolutePath(), emptyDest, StandardCopyOption.REPLACE_EXISTING);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
 
 }
