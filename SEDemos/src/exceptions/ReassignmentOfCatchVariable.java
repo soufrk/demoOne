cff7160e2822e9f5e7af30cb6fbb8e086f8afc9f
Souvik Goswami <soufrk@gmail.com>
2017-12-05 17:23:41 +0530

    Enhanced example.

--- SEDemos/src/exceptions/ReassignmentOfCatchVariable.java
+++ SEDemos/src/exceptions/ReassignmentOfCatchVariable.java
@@ -1,5 +1,6 @@
 package exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
@@ -14,17 +15,43 @@ public class ReassignmentOfCatchVariable {
 	try {
 	    throw new IOException();
 	} catch (IOException | ClassCastException e) {
+	    e = new ClassCastException();
+	    throw e;
+	}
+    }
+    
+    public static void main2(String[] args) throws Exception {
+	try {
+	    throw new IOException();
+	} catch (IOException | ClassCastException e) {
 	    e = new IOException();
 	    throw e;
 	}
     }
     
-    public static void main(String[] args) throws Exception {
+    public static void main3(String[] args) throws Exception {
+	try {
+	    throw new IOException();
+	} catch (IOException e) {
+	    e = new NullPointerException();
+	    throw e;
+	}
+    }
+    
+    public static void main4(String[] args) throws Exception {
+	try {
+	    throw new IOException();
+	} catch (IOException e) {
+	    e = new FileNotFoundException();
+	    throw e;
+	}
+    }
+    
+    public static void main5(String[] args) throws Exception {
 	try {
 	    throw new IOException();
 	} catch (IOException e) {
 	    e = new IOException();
-	    e = new NullPointerException();
 	    throw e;
 	}
     }
