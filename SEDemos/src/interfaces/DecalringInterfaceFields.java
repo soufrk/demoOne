48c9cb3db57db1a0bb1814753391ac4e40f04239
Souvik Goswami <soufrk@gmail.com>
2017-12-23 12:42:11 +0530

    Added new example in interface.

---
+++ SEDemos/src/interfaces/DecalringInterfaceFields.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+/**
+ * So which of the given declarations are valid ?
+ * 
+ * @author soufrk
+ *
+ */
+public interface DecalringInterfaceFields {
+    public static final int a = 0;
+    public final int b = 1;
+    public static int c = 3;
+    public final int d;
+    public int f;
+}
