4bb5283ef62a968ad0b31f51801e3df3043c9849
Souvik Goswami <soufrk@gmail.com>
2017-12-23 12:46:42 +0530

    Added new class for floation.

---
+++ SEDemos/src/basics/datatypes/FloatingCorrectly.java
@@ -0,0 +1,17 @@
+package basics.datatypes;
+
+/**
+ * One of these compiles. Guess which one.
+ * Plus one, if you can guess the error message.
+ * 
+ * @author soufrk
+ *
+ */
+public class FloatingCorrectly {
+
+    public static void main(String[] args) {
+	float a = 0;
+	float b = 0.0;
+    }
+
+}
