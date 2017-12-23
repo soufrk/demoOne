ff962e5416c60d5c8de8aa6d0d25848706026c3e
Souvik Goswami <soufrk@gmail.com>
2017-12-23 15:22:08 +0530

    Adding new class and package.

---
+++ SEDemos/src/io/serialization/ObjectOverwrite.java
@@ -0,0 +1,51 @@
+package io.serialization;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/**
+ * Predict what value gets printed as output.
+ * 
+ * @author soufrk
+ *
+ */
+public class ObjectOverwrite {
+
+    public static void main(String[] args) {
+	MyClass obj = new MyClass();
+	obj.x = 10;
+	obj.y = 20.0d;
+	obj.z = "A";
+	
+	try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("MyClass.ser"))){
+	    out.writeObject(obj);
+	    obj.x = 11;
+	    out.writeObject(obj);
+	} catch(IOException e){
+	    e.printStackTrace();
+	}
+	
+	try(ObjectInputStream out = new ObjectInputStream(new FileInputStream("MyClass.ser"))){
+	    obj = (MyClass) out.readObject();
+	    System.out.println(obj.x);
+	} catch(IOException | ClassNotFoundException e){
+	    e.printStackTrace();
+	}
+
+    }
+
+}
+
+class MyClass implements Serializable{
+
+    private static final long serialVersionUID = 7673510498952169540L;
+    
+    int x;
+    double y;
+    String z;
+    
+}
