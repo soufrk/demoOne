527f525bec4bd81273de55349118926823520368
Souvik Goswami <soufrk@gmail.com>
2017-12-23 13:31:52 +0530

    Modified class.

--- SEDemos/src/generics/Hierarchy.java
+++ SEDemos/src/generics/Hierarchy.java
@@ -1,17 +1,21 @@
 package generics;
 
 public class Hierarchy {
-
 }
 
-class BaseParent{
-	
+class BaseParent {
+    @Override
+    public String toString() {
+	return "Base Parent[]";
+    }
+    
+    public String thisHierarchy(){
+	return "this hierarchy:Base Parent[]";
+    }
 }
 
-class BaseChild1 extends BaseParent{
-	
+class BaseChild1 extends BaseParent {
 }
 
-class BaseChild2 extends BaseParent{
-	
+class BaseChild2 extends BaseParent {
 }
