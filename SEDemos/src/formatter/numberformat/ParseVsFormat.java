985f77207cbffbcfcf984b47623a2b8b3d35cbf4
Souvik Goswami <soufrk@gmail.com>
2017-12-29 12:25:32 +0530

    Updated class
    Added more example

--- SEDemos/src/formatter/numberformat/ParseVsFormat.java
+++ SEDemos/src/formatter/numberformat/ParseVsFormat.java
@@ -10,7 +10,7 @@ import java.text.NumberFormat;
  */
 public class ParseVsFormat {
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
 	String s = "987.654321";
 	double d = 987.654321d;
 	NumberFormat nf = NumberFormat.getInstance();
@@ -21,7 +21,20 @@ public class ParseVsFormat {
 	} catch (Exception e) {
 	    System.out.println("Exception");
 	}
-
+    }
+    
+    public static void main2(String[] args) {
+	String inputs[] = {"0.12345", "1.23546F"};
+	NumberFormat format = NumberFormat.getInstance();
+	format.setMaximumFractionDigits(2);
+	for(String input:inputs){
+	    try {
+		System.out.println(format.parse(input));
+		System.out.println(format.format(input));
+	    } catch (ParseException e) {
+		e.printStackTrace();
+	    }
+	}
     }
 
 }
