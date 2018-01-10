6e44531da38868a6f78102cd020272f67f0f94e4
Souvik Goswami <soufrk@gmail.com>
2018-01-10 16:25:43 +0530

    Added new class

---
+++ SEDemos/src/threads/TheNeverEndingThreadProblem.java
@@ -0,0 +1,28 @@
+package threads;
+
+/**
+ * Can we invoke start() multiple times on the same thread object
+ * 
+ * @author soufrk
+ *
+ */
+public class TheNeverEndingThreadProblem extends Thread {
+
+    public static void main(String[] args) {
+	Thread t = new TheNeverEndingThreadProblem();
+	try {
+	    while (true) {
+		t.start();
+		t.sleep(5000);
+	    }
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    @Override
+    public void run() {
+	this = new TheNeverEndingThreadProblem();
+    }
+
+}
