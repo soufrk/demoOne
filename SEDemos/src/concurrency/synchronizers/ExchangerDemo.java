88d9f788a6aeabf203d31b32e28f17dfe9268f1c
Souvik Goswami <soufrk@gmail.com>
2018-01-13 12:49:37 +0530

    Added new file

---
+++ SEDemos/src/concurrency/synchronizers/ExchangerDemo.java
@@ -0,0 +1,53 @@
+package concurrency;
+
+import java.util.concurrent.Exchanger;
+
+/**
+ * Simple demonstration of usage of an exchanger.
+ * 
+ * Use-case:Two threads exchanging String values to each oter.
+ *  
+ * @author soufrk
+ *
+ */
+public class ExchangerDemo extends Thread{
+    
+    // Instantiating an Exchanger
+    private static Exchanger<String> EXCHANGER = new Exchanger<>();
+    private String message;
+    
+    public ExchangerDemo(String message){
+	this.message = message;
+    }
+    
+    public static void main1(String[] args) {
+	Thread t1 = new ExchangerDemo("A");
+	Thread t2 = new ExchangerDemo("B");
+	t1.start();
+	t2.start();
+    }
+
+    public static void main2(String[] args) {
+	Thread t1 = new ExchangerDemo("A");
+	Thread t2 = new ExchangerDemo("B");
+	t1.start();
+	//t2.start();
+	try {
+	    t1.join();
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	}
+    }
+    
+    @Override
+    public void run() {
+        try {
+	    String reply = EXCHANGER.exchange(message);// This is a blocking call
+	    // to get message from exchanger.
+	    System.out.println(reply);
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	}
+    }
+
+}
