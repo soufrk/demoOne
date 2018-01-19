dc8946942573ca188147103db7c28776e8f3236f
Souvik Goswami <soufrk@gmail.com>
2018-01-19 12:58:59 +0530

    Renamed class.

---
+++ SEDemos/src/formatter/PrintUnicodeCharacter.java
@@ -0,0 +1,24 @@
+package formatter;
+
+/**
+ * Attempt to print a Unicode character on console.
+ * 
+ * @author soufrk
+ *
+ */
+public class PrintUnicodeCharacter {
+
+    public static void main(String[] args) {
+	// System.out.printf("%c", 97);
+	System.out.format("%1$c", 95);
+	System.out.format("%1$c", 175);
+	System.out.format("%1$c", 172);
+	System.out.format("%1$c", 255);
+	System.out.format("%1$c", 256);
+	System.out.format("%1$c", 257);
+	/*
+	 * Anything above 255 results in ?
+	 */
+    }
+
+}
