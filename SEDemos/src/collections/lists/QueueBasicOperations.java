d8fb1de9dc2694c89e6d1dedc948781e64ad13c0
Souvik Goswami <soufrk@gmail.com>
2018-01-22 14:06:25 +0530

    Added new class

---
+++ SEDemos/src/collections/lists/QueueBasicOperations.java
@@ -0,0 +1,24 @@
+package collections;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Basic functions around a queue.
+ * 
+ * @author soufrk
+ *
+ */
+public class QueueBasicOperations {
+
+    public static void main(String[] args) {
+	Queue<String> q = new LinkedList<>();
+	// 1. Add just like list.
+	System.out.println("Added element:" + q.add("A") + ", And the Q:" + q);
+	// 2. Remove an element.
+	System.out.println("Last element:" + q.remove() + ", And the Q:" + q);
+	// 3. Removing element from empty Q throws an exception.
+	q.remove();
+    }
+    
+}
