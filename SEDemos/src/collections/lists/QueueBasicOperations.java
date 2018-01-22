f0339dcccd319543262b85c8f9cc458cb261ac00
Souvik Goswami <soufrk@gmail.com>
2018-01-22 14:39:52 +0530

    Updated class

--- SEDemos/src/collections/lists/QueueBasicOperations.java
+++ SEDemos/src/collections/lists/QueueBasicOperations.java
@@ -11,8 +11,19 @@ import java.util.Queue;
  */
 public class QueueBasicOperations {
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
 	Queue<String> q = new LinkedList<>();
+	// 1. Add just like list.
+	System.out.println("Added element:" + q.add("A") + ", And the Q:" + q);
+	// 2. Remove an element.
+	System.out.println("Last element:" + q.remove() + ", And the Q:" + q);
+	// 3. Removing element from empty Q throws an exception.
+	q.remove();
+    }
+    
+    // But is the same valid with an array-list ?
+    public static void main(String[] args) {
+	Queue<String> q = new ArrayList<>();
 	// 1. Add just like list.
 	System.out.println("Added element:" + q.add("A") + ", And the Q:" + q);
 	// 2. Remove an element.
