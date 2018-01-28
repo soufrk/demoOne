2439dea2e8db8ef27d708eea9d0d146012bf1ef9
Souvik Goswami <soufrk@gmail.com>
2018-01-28 13:38:18 +0530

    Added new class

---
+++ SEDemos/src/cloning/AreClonedObjectsEqual.java
@@ -0,0 +1,25 @@
+package colne;
+
+/**
+ * So if an object is cloned,<ul>
+ * <li>are the two objects same ?</li>
+ * <li>does the cloned become equal to the real object?</li>
+ * </ul>
+ * 
+ * @author soufrk
+ *
+ */
+public class AreClonedObjectsEqual implements Cloneable {
+
+    public static void main(String[] args) {
+	AreClonedObjectsEqual object1 = new AreClonedObjectsEqual();
+	try {
+	    AreClonedObjectsEqual object2 = (AreClonedObjectsEqual) object1.clone();
+	    System.out.println(object1 == object2);
+	    System.out.println(object1.equals(object2));
+	} catch (CloneNotSupportedException e) {
+	    e.printStackTrace();
+	}
+    }
+
+}
