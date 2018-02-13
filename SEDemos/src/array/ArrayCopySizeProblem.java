bdee3cfc814d295cef74714c772ed1f5b6fe9844
Souvik Goswami <soufrk@gmail.com>
2018-02-13 14:55:24 +0530

    Updated class

--- SEDemos/src/array/ArrayCopySizeProblem.java
+++ SEDemos/src/array/ArrayCopySizeProblem.java
@@ -10,12 +10,21 @@ import java.util.Arrays;
  */
 public class ArrayCopySizeProblem {
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
 	int []source = {1, 2, 3};
 	int []destination = new int[4];
 	destination = Arrays.copyOf(source, 3);
 	destination[3] = 4;
-	System.out.println(destination.length);
+	System.out.println(Arrays.toString(destination));
+    }
+    
+    public static void main2(String[] args) {
+	int []source = {1, 2, 3};
+	int []destination = new int[4];
+	destination = Arrays.copyOf(source, 4);
+	System.out.println(Arrays.toString(destination));
+	destination[3] = 4;
+	System.out.println(Arrays.toString(destination));
     }
 
 }
