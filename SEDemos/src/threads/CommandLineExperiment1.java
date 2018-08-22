d70f64afe6675b349a4026bd6d605bdc91ab2c2b
Souvik Goswami <soufrk@gmail.com>
2018-08-22 13:40:29 +0530

    updated code format

--- SEDemos/src/threads/CommandLineExperiment1.java
+++ SEDemos/src/threads/CommandLineExperiment1.java
@@ -8,45 +8,49 @@ import java.io.Console;
  * @author soufrk
  *
  */
-public class CommandLineExperiment1 implements Runnable{
-	
-	private static int i;
+public class CommandLineExperiment1 implements Runnable {
 
-	public static void main(String[] args) {
-		Console c = System.console();
-		String n = "";
-		Thread t = new Thread(new CommandLineExperiment1());
-		do{
-			System.out.println("1. Read value");
-			System.out.println("2. Write value");
-			System.out.println("Exit(e|E)");
-			n = c.readLine();
-			if(n.equalsIgnoreCase("e"))
-				break;
-			try{
-				i = Integer.parseInt(n);
-				switch(i){
-				case 1:
-					t.start();
-					System.out.println("i=" + i);
-					System.out.println("i++=" + i++);
-					break;
-				case 2:System.out.println("Enter value:");
-				i = Integer.parseInt(c.readLine());break;
-				default: System.out.println("Invalid input");break;
-				}
-			} catch(NumberFormatException e){
-				System.out.println("Invalid input");
-			}
-		}while(!n.equalsIgnoreCase("e"));
-	}
+    private static int i;
 
-	@Override
-	public void run() {
-		int counter = 50000;
-		while(counter-- >0){
-			i++;
+    public static void main(String[] args) {
+	Console c = System.console();
+	String n = "";
+	Thread t = new Thread(new CommandLineExperiment1());
+	do {
+	    System.out.println("1. Read value");
+	    System.out.println("2. Write value");
+	    System.out.println("Exit(e|E)");
+	    n = c.readLine();
+	    if (n.equalsIgnoreCase("e"))
+		break;
+	    try {
+		i = Integer.parseInt(n);
+		switch (i) {
+		case 1:
+		    t.start();
+		    System.out.println("i=" + i);
+		    System.out.println("i++=" + i++);
+		    break;
+		case 2:
+		    System.out.println("Enter value:");
+		    i = Integer.parseInt(c.readLine());
+		    break;
+		default:
+		    System.out.println("Invalid input");
+		    break;
 		}
+	    } catch (NumberFormatException e) {
+		System.out.println("Invalid input");
+	    }
+	} while (!n.equalsIgnoreCase("e"));
+    }
+
+    @Override
+    public void run() {
+	int counter = 50000;
+	while (counter-- > 0) {
+	    i++;
 	}
+    }
 
 }
