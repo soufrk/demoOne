85dbc48648a8d5fa67750085bcb20687a4484d3a
Souvik Goswami <soufrk@gmail.com>
2021-05-24 19:45:02 +0530

    updated example

--- SEDemos/src/streams/Streams002.java
+++ SEDemos/src/streams/Streams002.java
@@ -5,45 +5,52 @@ import java.util.function.DoubleBinaryOperator;
 import java.util.stream.DoubleStream;
 
 /**
+ * Map-Reduction
+ * 
  * Given an array of double values, construct a stream-pipeline that return sum
  * of elements.
+ * 
  * @author soufrk
  */
 public class Streams002 {
 
     public static void main(String... args) {
         DoubleBinaryOperator sumOperator = new DoubleBinaryOperator() {
-
-            @Override
-            public double applyAsDouble(double left, double right) {
-                //System.out.println("Left=" + left);
-                //System.out.println("Right=" + right);
-                double sum = right + left;
-                return sum;
-            }
+            
+			@Override
+			public double applyAsDouble(double left, double right) {
+				//System.out.println("Left=" + left);
+				//System.out.println("Right=" + right);
+				double sum = right + left;
+				return sum;
+			}
         };
-
+       
         double[] values = {1.00, 2.00, 3.00, 4.00};
-        //double[] values = {};
+        //double[] values = {}; // What will be the output, if an empty array is passed ?
+        
+        // 1. Counting number of elements in stream
         long count = DoubleStream.of(values)
                 .map(v->v)
                 .count();
         System.out.println(count);
-
-        // Adding elements using sum()
+        
+        // 2. Adding elements using sum()
         double result = DoubleStream.of(values)
                 .map(v->v)
                 .sum();
-        //System.out.println(result);
-
-        // Using explicit reduce method
+        System.out.println(result);
+        
+        // 3. Using explicit reduce method
         OptionalDouble sum = DoubleStream.of(values)
         		.reduce(sumOperator);
         if(sum.isPresent()) {
         	System.out.println(sum.getAsDouble());
         }
-
-        // Using identity reduce
+        // 3.1 Alternate way of using Optional
+        sum.ifPresent(System.out::println);
+        
+        // 4. Using identity reduce
         double sum2 = DoubleStream.of(values)
         		//.reduce(0.0, (v1, v2)-> v1+v2);
         		.reduce(0.0, sumOperator);
