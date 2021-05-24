013d82d0dadbf31bbc35f7445dfec36f51c20cad
Souvik Goswami <soufrk@gmail.com>
2021-05-24 19:44:07 +0530

    updated example

--- SEDemos/src/streams/Streams001.java
+++ SEDemos/src/streams/Streams001.java
@@ -13,13 +13,15 @@ public class Streams001 {
 
     public static void main(String... args) {
         double[] values = {1.00, 2.00, 3.00, 4.00};
+        int[] numbers = {12, 13};
         List<Double> listOfDouble = Arrays.asList(1.00, 2.00, 3.00, 4.00); 
         countElementInsTream(Stream.of(values));
         countElementInsTream(Arrays.stream(values));
+        countElementInsTream(Arrays.stream(numbers));
         countElementInsTream(Stream.of(listOfDouble));
         countElementInsTream(listOfDouble.stream());
     }
-
+    
     private static void countElementInsTream(Stream<?> stream) {
     	long result = stream
     			.peek(e->System.out.println(e))
