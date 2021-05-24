054590a26ae19cecb8dfe30e793e19098054251b
Souvik Goswami <soufrk@gmail.com>
2021-05-24 19:45:39 +0530

    updated example

--- SEDemos/src/streams/Streams003.java
+++ SEDemos/src/streams/Streams003.java
@@ -5,32 +5,36 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * So, how will a Stream handle null value ?
+ * reduce() Map-Reduction
+ * 
+ * So, how will a Stream handle null value ? 
  * a. will the sum hold null value ?
- * b. will sum store Nullable Optional value
- * c. will the cde throw NullPointerException
- * d. the code won't compile ?
- *
+ * b. will sum store Nullable Optional value 
+ * c. will the cde throw NullPointerException ?
+ * d. the code won't compile
+ * 
  * @author soufrk
  *
  */
 public class Streams003 {
 
-    public static void main(String[] args) {
-        List<Double> listOfValues = new LinkedList(){{
-                add(1.2);
-                add(2.3);
-                add(3.4);
-                add(4.5);
-                add(5.6);
-                add(6.7);
-                add(null);
-            }};
-        Optional<Double> sum = listOfValues.stream()
-            .reduce((v1,v2)->{
-                return v1 + v2;
-            });
-        System.out.println(sum.get());
-    }
+	public static void main(String[] args) {
+		List<Double> listOfValues = new LinkedList() {
+			{
+				add(1.2);
+				add(2.3);
+				add(3.4);
+				add(4.5);
+				add(5.6);
+				add(6.7);
+				add(null);
+			}
+		};
+		Optional<Double> sum = listOfValues.stream()
+				.reduce((v1, v2) -> {
+					return v1 + v2;
+				});
+		System.out.println(sum.get());
+	}
 
 }
