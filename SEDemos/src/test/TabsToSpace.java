95345e79102356116ca9da025d59d871fbf7fab7
Souvik Goswami <soufrk@gmail.com>
2021-06-07 17:34:06 +0530

    completed writing of edited file

--- SEDemos/src/test/TabsToSpace.java
+++ SEDemos/src/test/TabsToSpace.java
@@ -1,55 +1,53 @@
-package test;
-
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.Iterator;
 
-/**
- * For all files in given directory,
- * convert tabs to 4 space.
- * 
- * @author soufrk
- *
- */
-public class TabsToSpace {
+public class Test {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("C:\\Users\\sogoswami\\Desktop");
+        String glob = "*.java";
+        DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path, glob);
+        Iterator<Path> fileIterator = directoryStream.iterator();
+        while(fileIterator.hasNext()) {
+            covertTabs(fileIterator.next());
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		Path path = Paths.get("C:\\Users\\MANALI\\Desktop");
-		String glob = "*.java";
-		DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path, glob);
-		Iterator<Path> fileIterator = directoryStream.iterator();
-		while(fileIterator.hasNext()) {
-			covertTabs(fileIterator.next());
-		}
-	}
-
-	private static void covertTabs(Path next) throws IOException {
-		BufferedInputStream bs = new BufferedInputStream(Files.newInputStream(next, StandardOpenOption.READ));
-		System.out.format("===Filename:%-20s===\n", next.getFileName());
-		StringBuilder contents = new StringBuilder();
-		int read = -1, line = 0, column = 0;
-		do {
-			read = bs.read();
-			switch(read) {
-			case '\t':
-				System.out.format("Tab at line:%d, column:%d\n", line, column);
-				break;
-			case '\n':
-				column = 0;
-				line++;
-				break;
-			default:
-				column++;
-			}
-			if(read == '\t') {
-				System.out.println("Space");
-			}
-		} while(read> -1);
-	}
-
-}
+    private static void covertTabs(Path next) throws IOException {
+        BufferedInputStream bs = new BufferedInputStream(Files.newInputStream(next, StandardOpenOption.READ));
+        System.out.format("===Filename:%-20s===\n", next.getFileName());
+        ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        int read = -1, line = 0, column = 0;
+        do {
+            read = bs.read();
+            //switch(read) {
+            //case '\t': // If TAB is encountered
+            //    System.out.format("Tab at line:%d, column:%d\n", line, column); // DEBUG
+            //    break;
+            //case '\n':
+            //    column = 0;
+            //    line++;
+            //    break;
+            //default:
+            //    column++;
+            //}
+            if(read == '\t') {
+                //System.out.println("Space");
+                buf.write("    ".getBytes());
+            } else {
+                buf.write((byte)read);
+            }
+        } while(read> -1);
+        System.out.format("%s\n%s\n", "=======READ FILE CONTENTS========", buf.toString("UTF-8"));
+        Path editedFile = Files.write(Paths.get(next.toAbsolutePath().toString() + "-edited"), 
+            buf.toByteArray(), StandardOpenOption.WRITE, StandardOpenOption.CREATE, 
+            StandardOpenOption.TRUNCATE_EXISTING);
+        System.out.format("%s\n%s\n", "=======EDITED FILE ========", editedFile.toAbsolutePath().toString());
+    }
+}ÿ
