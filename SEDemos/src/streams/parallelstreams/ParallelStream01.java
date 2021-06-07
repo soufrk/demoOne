c656b7be8cadf51e2f02ca58ab9356056b392649
Souvik Goswami <soufrk@gmail.com>
2021-06-07 17:39:37 +0530

    updated file

--- SEDemos/src/streams/parallelstreams/ParallelStream01.java
+++ SEDemos/src/streams/parallelstreams/ParallelStream01.java
@@ -1,5 +1,18 @@
 package streams.parallelstreams;
 
+import java.util.Arrays;
+
 public class ParallelStream01 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 6, 2, 9, 10};
+        // Normal Stream operation
+        int sum1 = Arrays.stream(numbers).sum();
+        System.out.println("Sum=" + sum1);
+
+        // Parallel Stream operation
+        int sum2 = Arrays.stream(numbers).parallel().sum();
+        System.out.println("Sum=" + sum2);
+    }
 
 }
