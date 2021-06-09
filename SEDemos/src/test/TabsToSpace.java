3d3463b862fb83e0f565ddd6ce946fa9cc7190d7
Souvik Goswami <soufrk@gmail.com>
2021-06-09 16:12:41 +0530

    corrected logic

--- SEDemos/src/test/TabsToSpace.java
+++ SEDemos/src/test/TabsToSpace.java
@@ -1,16 +1,20 @@
+package test;
+
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.charset.Charset;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.Iterator;
 
-public class Test {
+public class TabsToSpace {
+	
     public static void main(String[] args) throws IOException {
-        Path path = Paths.get("C:\\Users\\sogoswami\\Desktop");
+        Path path = Paths.get("C:\\Users\\MANALI\\Desktop");
         String glob = "*.java";
         DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path, glob);
         Iterator<Path> fileIterator = directoryStream.iterator();
@@ -23,9 +27,10 @@ public class Test {
         BufferedInputStream bs = new BufferedInputStream(Files.newInputStream(next, StandardOpenOption.READ));
         System.out.format("===Filename:%-20s===\n", next.getFileName());
         ByteArrayOutputStream buf = new ByteArrayOutputStream();
-        int read = -1, line = 0, column = 0;
-        do {
-            read = bs.read();
+//        StringBuilder sb = new StringBuilder(); 
+        int read = 0, line = 0, column = 0;
+        read = bs.read();
+        while(read > -1) {
             //switch(read) {
             //case '\t': // If TAB is encountered
             //    System.out.format("Tab at line:%d, column:%d\n", line, column); // DEBUG
@@ -43,11 +48,16 @@ public class Test {
             } else {
                 buf.write((byte)read);
             }
-        } while(read> -1);
-        System.out.format("%s\n%s\n", "=======READ FILE CONTENTS========", buf.toString("UTF-8"));
+//            sb.append(read);
+            read = bs.read();
+        } //while(read> -1);
+//        System.out.format("%s\n%s\n", "======= READ FILE ========", sb.toString());
+        System.out.format("%s\n%s\n", "======= READ FILE CONTENTS ========", buf.toString(
+        		Charset.defaultCharset().name()
+        		));
         Path editedFile = Files.write(Paths.get(next.toAbsolutePath().toString() + "-edited"), 
             buf.toByteArray(), StandardOpenOption.WRITE, StandardOpenOption.CREATE, 
             StandardOpenOption.TRUNCATE_EXISTING);
         System.out.format("%s\n%s\n", "=======EDITED FILE ========", editedFile.toAbsolutePath().toString());
     }
-}ÿ
+}
